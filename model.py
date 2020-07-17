f1a3e8928774ca028b29045a6d2a854628205f18
Yiding Hao <yiding.hao@yale.edu>
2020-07-17 17:17:27 -0500

    Edited some docstrings

--- model.py
+++ model.py
@@ -3,7 +3,6 @@ from typing import Tuple
 
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 
 
 class RNNModel(nn.Module):
@@ -96,75 +95,76 @@ class RNNModel(nn.Module):
             return weight.new_zeros(self.nlayers, batch_size, self.nhid)
 
 
-# Temporarily leave PositionalEncoding module here. Will be moved somewhere
-# else.
 class PositionalEncoding(nn.Module):
-    r"""Inject some information about the relative or absolute position of
-    the tokens
-        in the sequence. The positional encodings have the same dimension as
-        the embeddings, so that the two can be summed. Here, we use sine and
-        cosine
-        functions of different frequencies.
-    .. math::
-        \text{PosEncoder}(pos, 2i) = sin(pos/10000^(2i/d_model))
-        \text{PosEncoder}(pos, 2i+1) = cos(pos/10000^(2i/d_model))
-        \text{where pos is the word position and i is the embed idx)
-    Args:
-        d_model: the embed dim (required).
-        dropout: the dropout value (default=0.1).
-        max_len: the max. length of the incoming sequence (default=5000).
-    Examples:
-        >>> pos_encoder = PositionalEncoding(d_model)
+    """
+    Positional encoding for the Transformer, implemented as a Module.
     """
 
-    def __init__(self, d_model, dropout=0.1, max_len=5000):
+    def __init__(self, d_model: int, dropout: float = 0.1,
+                 max_len: int = 5000):
+        """
+        Constructor for a positional encoding.
+
+        :param d_model: The Transformer's input size
+        :param dropout: The dropout rate
+        :param max_len: The maximum sequence length
+        """
         super(PositionalEncoding, self).__init__()
         self.dropout = nn.Dropout(p=dropout)
 
+        # Create the positional encoding matrix
         pe = torch.zeros(max_len, d_model)
         position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
-        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (
-            -math.log(10000.0) / d_model))
+        div_term = torch.exp(torch.arange(0, d_model, 2).float() *
+                             (-math.log(10000.0) / d_model))
         pe[:, 0::2] = torch.sin(position * div_term)
         pe[:, 1::2] = torch.cos(position * div_term)
         pe = pe.unsqueeze(0).transpose(0, 1)
-        self.register_buffer('pe', pe)
+
+        self.register_buffer("pe", pe)
 
     def forward(self, x):
-        r"""Inputs of forward function
-        Args:
-            x: the sequence fed to the positional encoder model (required).
-        Shape:
-            x: [sequence length, batch size, embed dim]
-            output: [sequence length, batch size, embed dim]
-        Examples:
-            >>> output = pos_encoder(x)
         """
+        Adds positional encoding to a tensor.
 
+        :param x: A tensor
+        :return: x, with positional encoding added
+        """
         x = x + self.pe[:x.size(0), :]
         return self.dropout(x)
 
 
 class TransformerModel(nn.Module):
-    """Container module with an encoder, a recurrent or transformer module,
-    and a decoder."""
+    """
+    Container module with an encoder, a transformer module, and a
+    decoder.
+    """
 
-    def __init__(self, ntoken, ninp, nhead, nhid, nlayers, dropout=0.5):
+    def __init__(self, ntoken: int, ninp: int, nhead: int, nhid: int,
+                 nlayers: int, dropout: float = 0.5):
+        """
+        Constructor for a Transformer model.
+
+        :param ntoken: The vocab size
+        :param ninp: The embedding size
+        :param nhead: The number of attention heads
+        :param nhid: The hidden size
+        :param nlayers: The number of layers
+        :param dropout: The dropout rate
+        """
         super(TransformerModel, self).__init__()
-        try:
-            from torch.nn import TransformerEncoder, TransformerEncoderLayer
-        except:
-            raise ImportError(
-                'TransformerEncoder module does not exist in PyTorch 1.1 or '
-                'lower.')
-        self.model_type = 'Transformer'
-        self.src_mask = None
-        self.pos_encoder = PositionalEncoding(ninp, dropout)
-        encoder_layers = TransformerEncoderLayer(ninp, nhead, nhid, dropout)
-        self.transformer_encoder = TransformerEncoder(encoder_layers, nlayers)
-        self.encoder = nn.Embedding(ntoken, ninp)
+        self.model_type = "Transformer"
         self.ninp = ninp
+
+        self.encoder = nn.Embedding(ntoken, ninp)
         self.decoder = nn.Linear(ninp, ntoken)
+        self.pos_encoder = PositionalEncoding(ninp, dropout)
+        self.src_mask = None
+
+        # Initialize Transformer layers
+        encoder_layers = nn.TransformerEncoderLayer(ninp, nhead, nhid, dropout)
+        self.transformer_encoder = nn.TransformerEncoder(encoder_layers,
+                                                         nlayers)
 
         self.init_weights()
 
@@ -175,10 +175,8 @@ class TransformerModel(nn.Module):
         return mask
 
     def init_weights(self):
-        initrange = 0.1
-        nn.init.uniform_(self.encoder.weight, -initrange, initrange)
-        nn.init.zeros_(self.decoder.weight)
-        nn.init.uniform_(self.decoder.weight, -initrange, initrange)
+        nn.init.xavier_uniform_(self.encoder.weight)
+        nn.init.xavier_uniform_(self.decoder.weight)
 
     def forward(self, src, has_mask=True):
         if has_mask:
@@ -193,5 +191,4 @@ class TransformerModel(nn.Module):
         src = self.encoder(src) * math.sqrt(self.ninp)
         src = self.pos_encoder(src)
         output = self.transformer_encoder(src, self.src_mask)
-        output = self.decoder(output)
-        return F.log_softmax(output, dim=-1)
+        return self.decoder(output)
